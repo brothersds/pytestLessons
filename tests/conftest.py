f26480984ab30d0d7db98bc1f677dde86db2c0a0
Dmitry <brothersds@ngs.ru>
2022-12-04 19:36:02 +0300

    end_test_lesson_4

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,10 @@ def set_up():
     print("\nВход в систему выполнен")
     yield
     print("\nВыход из системы")
+
+
+@pytest.fixture(scope="module")
+def some():
+    print("\nНачало")
+    yield
+    print("\nКонец")
--- tests/test_mail_1.py
+++ tests/test_mail_1.py
@@ -1,8 +1,8 @@
 
 
-def test_sending_mail_1(set_up):
+def test_sending_mail_1(set_up, some):
     print("Письмо отправлено")
 
 
-def test_sending_mail_2(set_up):
+def test_sending_mail_2(set_up, some):
     print("Письмо отправлено")
