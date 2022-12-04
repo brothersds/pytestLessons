77a6cb40c700f0c88fb150319fbd51cd67b9a97e
Dmitry <brothersds@ngs.ru>
2022-12-04 19:01:01 +0300

    end_test_lesson_1

---
+++ tests/test_mail_1.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture()
+def set_up():
+    print("\nВход в систему выполнен")
+
+
+def test_sending_mail_1(set_up):
+    print("Письмо отправлено")
+
+
+def test_sending_mail_2(set_up):
+    print("Письмо отправлено")
