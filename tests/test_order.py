f30548877f088ef77be8382560c13abf04860523
Dmitry <brothersds@ngs.ru>
2022-12-04 20:02:01 +0300

    end_test_lesson_5

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,24 +1,31 @@
+import pytest
 
 
-def test_method_1(set_up):
+@pytest.mark.run(order=2)
+def test_method_1():
     print("Метод 1")
 
 
-def test_method_2(set_up):
+@pytest.mark.run(order=1)
+def test_method_2():
     print("Метод 2")
 
 
-def test_method_3(set_up):
+@pytest.mark.run(order=6)
+def test_method_3():
     print("Метод 3")
 
 
-def test_method_4(set_up):
+@pytest.mark.run(order=4)
+def test_method_4():
     print("Метод 4")
 
 
-def test_method_5(set_up):
+@pytest.mark.run(order=3)
+def test_method_5():
     print("Метод 5")
 
 
-def test_method_6(set_up):
+@pytest.mark.run(order=5)
+def test_method_6():
     print("Метод 6")
